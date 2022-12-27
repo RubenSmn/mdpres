c5c00401713dad6375652f1be6c9cbcebaf92f8b
RubenSmn <saaman.ruben@gmail.com>
2022-12-27 16:37:11 +0100

    feat: add line numbers and blur

--- src/Code.tsx
+++ src/Code.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 
-const Code: React.FC<any> = ({ children, data, className }) => {
+const Code: React.FC<any> = ({ children, data }) => {
+  let range = [0, children.length];
+  const lineOffset = 1;
+
+  if (data !== undefined && data.meta !== undefined) {
+    const ranges = data.meta.split("-").map((point: string) => parseInt(point));
+    range = ranges;
+  }
+
+  const isInRange = (value: number) => {
+    if (value >= range[0] && value < range[1]) return true;
+    return false;
+  };
+
   return (
     <code>
       {children.map((line: any, idx: number) => {
         return (
           <span
-            className="wenky-line"
-            style={{
-              display: "flex",
-              lineHeight: "24px",
-            }}
+            className={["code-line", !isInRange(idx) && "blurred"].join(" ")}
             key={`line-${idx}`}
           >
+            <span className="code-line line-number">{idx + lineOffset}</span>
             {line}
           </span>
         );
