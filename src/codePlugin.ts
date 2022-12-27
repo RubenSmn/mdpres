386ca8730361a2d93eb14210b8140882dd5e8bff
RubenSmn <saaman.ruben@gmail.com>
2022-12-27 21:10:09 +0100

    refactor: rename and add comments

--- src/codePlugin.ts
+++ src/codePlugin.ts
@@ -11,71 +11,87 @@ const specialChars: { [key: string]: string } = {
 
 export const codePlugin: any = () => {
   return function transformer(tree: any) {
-    const bold = (node: any) => {
+    const tokenize = (node: any) => {
       if (node.tagName !== "code") return;
 
+      // split the code into lines
       const lines = node.children[0].value.split(/\n\r?/g);
+      // remove the last empty line
       lines.pop();
 
+      // get the code language from the className
       const language =
         node.properties.className[0].replace("language-", "") || "";
 
+      // empty the node children
+      const formattedLines = [];
       node.children = [];
 
-      lines
-        .map((line: string) => {
-          return Prism.highlight(
-            line,
-            Prism.languages[language || "javascript"],
-            language,
-          );
-        })
-        .forEach((line: string) => {
-          const newLine = line.replace(/\s+(?=\<)/g, (match: string) => {
+      for (const line of lines) {
+        // highlight the current line
+        const highlightedLine = Prism.highlight(
+          line,
+          Prism.languages[language || "javascript"],
+          language,
+        );
+
+        // wrap the whitespaces into a span
+        const newLine = highlightedLine.replace(
+          /\s+(?=\<)/g,
+          (match: string) => {
             return `<span>${match}</span>`;
-          });
-          node.children.push({
-            type: "text",
-            value: newLine,
-          });
-          return newLine;
+          },
+        );
+
+        // add the new node to the array
+        formattedLines.push({
+          type: "text",
+          value: newLine,
         });
+      }
 
       const newNodeChildren = [];
 
-      for (const child of node.children) {
-        const test = child.value.split(/(?:<span(?: class=)?>?)|(?:<\/span)/g);
+      for (const line of formattedLines) {
+        // split the text of the line into chunks
+        const chunks = line.value.split(/(?:<span(?: class=)?>?)|(?:<\/span)/g);
 
+        // create an array for all the new nodes of the line
         const lineNodes = [];
 
-        for (const x of test) {
-          if (x === "") continue;
-          if (/^\s+$/.test(x)) {
+        for (const chunk of chunks) {
+          if (chunk === "" || chunk === ">") continue;
+
+          // check if chunk contains only whitespaces
+          if (/^\s+$/.test(chunk)) {
             lineNodes.push({
               type: "element",
               tagName: "span",
               children: [
                 {
                   type: "text",
-                  value: x,
+                  value: chunk,
                 },
               ],
             });
             continue;
           }
 
-          const newClassName = x.match(/(?<=").*(?=\")/);
-          const value = x.match(/(?<=\>).*$/g);
+          // get the className and the value from the chunk
+          const tokenClassName = chunk.match(/(?<=").*(?=\")/);
+          const value = chunk.match(/(?<=\>).*$/g);
 
-          const content = value[0] || null;
+          if (value === null) continue;
 
+          const content = value[0];
           if (content === null || content === "") continue;
 
+          // add a new node to the line with the corresponding text as a child
           lineNodes.push({
             type: "element",
             tagName: "span",
             properties: {
-              ...(newClassName && { className: [newClassName] }),
+              ...(tokenClassName && { className: [tokenClassName] }),
             },
             children: [
               {
@@ -89,24 +105,27 @@ export const codePlugin: any = () => {
           });
         }
 
+        // add the lineNodes to the parent lineNode
         const lineNode = {
           type: "element",
           tagName: "span",
           properties: {
-            className: ["wenky-line"],
+            className: ["code-line"],
           },
           children: lineNodes,
         };
 
+        // add the lineNode to the newNodeChildren
         newNodeChildren.push(lineNode);
       }
 
+      // set the newNodeChildren to the node's children
       node.children = newNodeChildren;
 
       if (node.data !== null || node.data !== undefined)
         node.properties["data"] = node.data;
     };
 
-    visit(tree, "element", bold);
+    visit(tree, "element", tokenize);
   };
 };
