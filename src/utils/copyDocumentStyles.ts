ea8038e0db4691e0ee4e462d7ea8444e1ab59ea5
RubenSmn <saaman.ruben@gmail.com>
2023-02-01 14:22:42 +0100

    fix: relative path styles not working on new window

--- src/utils/copyDocumentStyles.ts
+++ src/utils/copyDocumentStyles.ts
@@ -1,7 +1,16 @@
 export const copyDocumentStyles = (src: Document, dest: Document) => {
   Array.from(src.styleSheets).forEach((styleSheet: StyleSheet) => {
     if (styleSheet.ownerNode === null) return;
-    dest.head.appendChild(styleSheet.ownerNode.cloneNode(true));
+    const newStyleSheet = styleSheet.ownerNode.cloneNode(
+      true,
+    ) as HTMLLinkElement;
+
+    if (import.meta.env.DEV === false) {
+      // some issue with relative path
+      newStyleSheet.href = newStyleSheet.href;
+    }
+
+    dest.head.appendChild(newStyleSheet);
   });
   Array.from(src.fonts).forEach((font) => dest.fonts.add(font));
 };
