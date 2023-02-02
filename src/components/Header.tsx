095ced6da8264115bf7c1ed1e03097e0047b5e90
RubenSmn <saaman.ruben@gmail.com>
2023-02-02 15:44:54 +0100

    feat: add getting started link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <Link to="/">
-        <h2>MDPres</h2>
-      </Link>
-      <div className="header-links">
+      <div className="header-left">
+        <Link to="/">
+          <h2>MDPres</h2>
+        </Link>
+        <Link to="/getting-started">Getting Started</Link>
+      </div>
+      <div className="header-right">
         <a
           href="https://github.com/RubenSmn/mdpres"
           target={"_blank"}
